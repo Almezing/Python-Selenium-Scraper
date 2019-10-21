d76af6b4fa58f08bbac10b7352490f87bc3657de
Almezing <neomic180@gmail.com>
2019-10-21 15:23:11 -0400

    Update python-scraper.py

--- python-scraper.py
+++ python-scraper.py
@@ -2,6 +2,7 @@
 # https://pyinstaller.readthedocs.io/en/stable/spec-files.html#adding-files-to-the-bundle
 # os.path.dirname(os.path.abspath(__file__))
 # https://pyinstaller.readthedocs.io/en/stable/usage.html
+# https://brice-v.github.io/docs/BriceVadnaisResume.pdf
 
 import time
 import os,sys
